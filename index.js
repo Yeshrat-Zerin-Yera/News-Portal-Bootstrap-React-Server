91e2c53d6606e82923fd920c54b6d54400fe8185
unknown <yeshratzerinyera@gmail.com>
2022-12-01 19:35:08 +0600

    Safety Commit

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const port = process.env.PORT || 5000;
 const cors = require('cors');
 
 const categories = require('./data/categories.json');
+const allNews = require('./data/news.json');
 
 app.use(cors());
 
@@ -13,7 +14,28 @@ app.get('/', (req, res) => {
 
 app.get('/categories', (req, res) => {
     res.send(categories);
+});
+
+app.get('/categories/:id', (req, res) => {
+    const id = req.params.id;
+    if (id === '08') {
+        res.send(allNews);
+    }
+    else {
+        const categoryNews = allNews.filter(news => news.category_id === id);
+        res.send(categoryNews);
+    }
+});
+
+app.get('/news', (req, res) => {
+    res.send(allNews);
 })
+
+app.get('/news/:id', (req, res) => {
+    const id = req.params.id;
+    const selectedNews = allNews.find(news => news._id === id);
+    res.send(selectedNews);
+});
 
 app.listen(port, () => {
     console.log('News Portal Server Running On Port: ', port);
